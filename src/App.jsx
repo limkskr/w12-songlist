57f579834820ac8cfe14f072531331d1c8f9308d
limskskr <ksks3164@gmail.com>
2025-11-20 14:22:23 +0900

    Backend 접근

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import SongList from './pages/SongList.jsx'
+import SongDetail from './pages/SongDetail.jsx'
+import { getSongList } from './api/songApi'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [songs, setSongs] = useState([])
+
+  useEffect(() => {
+    const fetchSongs = async () => {
+      try {
+        const data = await getSongList()
+        setSongs(data)
+      } catch (err) {
+        console.error("Failed to fetch songs:", err)
+      }
+    }
+
+    fetchSongs()
+  }, []) //빈배열이 중요
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Routes>
+      <Route path="/" element={<SongList songs={songs} />} />
+      <Route path="/song/:id" element={<SongDetail songs={songs} />} />
+    </Routes>
   )
 }
 
